ee49b728b382e979bfb89122dce7a2cd48c270d3
Anna Kocheshkova <v-annkoc@microsoft.com>
2018-11-06 19:48:50 +0300

    Fixed logo styles.
    Added more space around logo;

--- Source/Assets/AppCenterEditorExtensions/Editor/Scripts/Panels/AppCenterEditorHeader.cs
+++ Source/Assets/AppCenterEditorExtensions/Editor/Scripts/Panels/AppCenterEditorHeader.cs
@@ -9,32 +9,34 @@ namespace AppCenterEditor
         {
             if (AppCenterEditorHelper.uiStyle == null)
                 return;
-
-            //using Begin Vertical as our container.
-            using (new AppCenterGuiFieldHelper.UnityHorizontal(GUILayout.Height(52)))
+            using (new AppCenterGuiFieldHelper.UnityHorizontal(AppCenterEditorHelper.uiStyle.GetStyle("gpStyleHeaderWrapper")))
             {
-                EditorGUILayout.LabelField("", AppCenterEditorHelper.uiStyle.GetStyle("acLogo"), GUILayout.MaxHeight(60), GUILayout.Width(60));
-                EditorGUILayout.LabelField("App Center", AppCenterEditorHelper.uiStyle.GetStyle("gpStyleGray2"), GUILayout.MinHeight(52));
-
-                float gmAnchor = EditorGUIUtility.currentViewWidth - 30;
-
-
-                if (EditorGUIUtility.currentViewWidth > 375)
+                //using Begin Vertical as our container.
+                using (new AppCenterGuiFieldHelper.UnityHorizontal(GUILayout.Height(52)))
                 {
-                    gmAnchor = EditorGUIUtility.currentViewWidth - 140;
-                    GUILayout.BeginArea(new Rect(gmAnchor, 10, 140, 42));
-                    GUILayout.BeginHorizontal();
-                }
-                else
-                {
-                    GUILayout.BeginArea(new Rect(gmAnchor, 10, EditorGUIUtility.currentViewWidth * .25f, 42));
-                    GUILayout.BeginHorizontal();
-                }
+                    EditorGUILayout.LabelField("", AppCenterEditorHelper.uiStyle.GetStyle("acLogo"), GUILayout.MaxHeight(60), GUILayout.Width(60));
+                    EditorGUILayout.LabelField("App Center", AppCenterEditorHelper.uiStyle.GetStyle("gpStyleGray2"), GUILayout.MinHeight(52));
 
-                GUILayout.EndHorizontal();
-                GUILayout.EndArea();
+                    float gmAnchor = EditorGUIUtility.currentViewWidth - 30;
 
-                //end the vertical container
+
+                    if (EditorGUIUtility.currentViewWidth > 375)
+                    {
+                        gmAnchor = EditorGUIUtility.currentViewWidth - 140;
+                        GUILayout.BeginArea(new Rect(gmAnchor, 10, 140, 42));
+                        GUILayout.BeginHorizontal();
+                    }
+                    else
+                    {
+                        GUILayout.BeginArea(new Rect(gmAnchor, 10, EditorGUIUtility.currentViewWidth * .25f, 42));
+                        GUILayout.BeginHorizontal();
+                    }
+
+                    GUILayout.EndHorizontal();
+                    GUILayout.EndArea();
+
+                    //end the vertical container
+                }
             }
 
             ProgressBar.Draw();
